5889b91b4b3dd671dbe09cde1321513442fd7c66
Gracie Evers <evers.gracie@gmail.com>
2022-02-10 13:09:25 -0600

    fixing some bugs and css

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
-function newCard() {
+function newCards() {
 
+    let request = new XMLHttpRequest();
+    request.open("GET", "card_data.json", true);
+    request.send();
+    request.onreadystatechange = (e) => {
+        quoteDataText = request.responseText;
+        let card_data = JSON.parse(request.responseText);
+        console.log(card_data.quote);
+
+        //Update the Page HTML
+        let quoteBoxElement = document.getElementById("cardDisplay")
+        quoteBoxElement.innerText = `"${card_data.quote}"`
+    }
 }
