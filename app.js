31082a7ce75c56471d1600a57d57c5b9e9a5eb1e
Gracie Evers <evers.gracie@gmail.com>
2022-02-11 19:32:39 -0600

    added cool css button

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
-function newCards() {
+const fs = require("fs");
+fs.readFile("./card_data.json", "utf8", (err, jsonString) => {
+    console.log(jsonString)
 
-    let request = new XMLHttpRequest();
-    request.open("GET", "card_data.json", true);
-    request.send();
-    request.onreadystatechange = (e) => {
-        quoteDataText = request.responseText;
-        let card_data = JSON.parse(request.responseText);
-        console.log(card_data.quote);
+});
 
-        //Update the Page HTML
-        let quoteBoxElement = document.getElementById("cardDisplay")
-        quoteBoxElement.innerText = `"${card_data.quote}"`
-    }
-}
+// function newCards() {
+
+//     let request = new XMLHttpRequest();
+//     request.open("GET", "card_data.json", true);
+//     request.send();
+//     request.onreadystatechange = (e) => {
+//         quoteDataText = request.responseText;
+//         let card_data = JSON.parse(request.responseText);
+//         console.log(card_data.quote);
+
+//         //Update the Page HTML
+//         let quoteBoxElement = document.getElementById("cardDisplay")
+//         quoteBoxElement.innerText = `"${card_data.quote}"`
+//     }
+// }
